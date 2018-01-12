91ffce79738fc559b8a3b1d51390cedc2f07f62f
alumno <lugmansito@gmail.com>
2018-01-12 12:08:06 +0100

    Cambios realizados

--- app/src/main/java/com/example/lugman/app_01_pelota/Gestion.java
+++ app/src/main/java/com/example/lugman/app_01_pelota/Gestion.java
@@ -1,10 +1,11 @@
 package com.example.lugman.app_01_pelota;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.On
 
 public class Gestion extends AppCompatActivity {
 
@@ -12,6 +13,9 @@ public class Gestion extends AppCompatActivity {
     private  int dificultad;
     private int FPS = 30;
     private Handler temporizador;
+    private int puntos = 0 ;
+    MediaPlayer golpe,finS;
+
 
 
 
@@ -25,6 +29,12 @@ public class Gestion extends AppCompatActivity {
 
         partida = new Partida(getApplicationContext(),dificultad);
         setContentView(partida);
+
+        temporizador = new Handler();
+        temporizador.postDelayed(elhilo,2000);
+
+        finS=MediaPlayer.create(this,R.raw.fin);
+        golpe=MediaPlayer.create(this,R.raw.golpe);
     }
     private  Runnable elhilo =  new Runnable() {
         @Override
@@ -42,11 +52,20 @@ public class Gestion extends AppCompatActivity {
     public boolean onTouchEvent (MotionEvent event){
         int x = (int) event.getX();
         int y = (int) event.getY();
-        partida.toque(x,y);
+        if(partida.toque(x,y)){
+        golpe.start();
+        puntos++;
+            MediaPlayer mediaPlayer= new MediaPlayer();
+
+        }
         return false;
     }
     public  void fin(){
         temporizador.removeCallbacks(elhilo);
+        Intent in =  new Intent(Gestion.this,MainActivity.class);
+        in.putExtra("TOQUES",puntos);
+        setResult(RESULT_OK,in);
         finish();
+        finS.start();
     }
 }
--- app/src/main/java/com/example/lugman/app_01_pelota/MainActivity.java
+++ app/src/main/java/com/example/lugman/app_01_pelota/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.lugman.app_01_pelota;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button ayuda,facil,dificil,normal;
+    TextView puntos;
+    int record;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button ayuda,facil,dificil,normal;
 
         ayuda=(Button) findViewById(R.id.ayuda);
         facil=(Button) findViewById(R.id.facil);
         dificil=(Button) findViewById(R.id.dificil);
         normal=(Button) findViewById(R.id.medio);
 
+        puntos =  (TextView) findViewById(R.id.textView);
+
+//        golpeo=MediaPlayer.create(this,android.R.raw.)
 
         ayuda.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,18 +44,17 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,Gestion.class);
                 switch (view.getId()){
                     case R.id.facil:
-                        intent.putExtra("DIFICULTAD",3);
+                        intent.putExtra("DIFICULTAD",1);
                         Log.d("CLick","3");
-
-                        startActivity(intent);
+                        startActivityForResult(intent,121);
                         break;
                     case R.id.medio:
-                        intent.putExtra("DIFICULTAD",3);
-                        startActivity(intent);
+                        intent.putExtra("DIFICULTAD",2);
+                        startActivityForResult(intent,121);
                         break;
                     case R.id.dificil:
                         intent.putExtra("DIFICULTAD",3);
-                        startActivity(intent);
+                        startActivityForResult(intent,121);
                         break;
                 default:
                     Log.d("CLick","Default");
@@ -57,9 +67,49 @@ public class MainActivity extends AppCompatActivity {
         normal.setOnClickListener(listener);
         dificil.setOnClickListener(listener);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode  == 121 &&  resultCode == RESULT_OK){
+            int toques;
+            toques = data.getExtras().getInt("TOQUES");
+            if (toques > record){
+                record = toques;
+                puntos.setText("PuntuaciónRecord  \n "+toques);
+                guarfaremosReord();
+
+            }else {
+                String puntuancion = "Toques hechos "+toques;
+                Toast mi_toas = Toast.makeText(this,puntuancion,Toast.LENGTH_LONG);
+                mi_toas.setGravity(Gravity.CENTER,0,0);
+                mi_toas.show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        leerRecord();
+
+    }
 
     private void ayuda() {
         Intent i =  new Intent(MainActivity.this,Ayuda.class);
         startActivity(i);
+    }
+
+
+    public void guarfaremosReord(){
+        SharedPreferences datos = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor mi_editor = datos.edit();
+        mi_editor.putInt("RECORD",record);
+        mi_editor.apply();
+    }
+    public void leerRecord(){
+        SharedPreferences datos = PreferenceManager.getDefaultSharedPreferences(this);
+        record=datos.getInt("RECORD",0);
+        puntos.setText("Puntuación  \n "+record);
+
     }
 }
